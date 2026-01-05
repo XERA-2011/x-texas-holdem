64c0fa70fc429025e02ff55a5c6127b794e36d64
🍀 <50286230+XERA-2011@users.noreply.github.com>
2026-01-05 09:52:15 +0800

    Project initialize

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        minHeight: "100vh",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1 style={{ fontSize: "2.25rem", fontWeight: "bold" }}>x2011</h1>
+    </div>
+  );
+}
